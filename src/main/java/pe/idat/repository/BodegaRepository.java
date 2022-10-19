69612629bcdec1c417a0850f8f10a57880979834
JulioRamirez <julio.ramirez@entelgy.com>
2022-10-18 22:09:18 -0500

    @

--- src/main/java/pe/idat/repository/BodegaRepository.java
+++ src/main/java/pe/idat/repository/BodegaRepository.java
@@ -1,9 +1,11 @@
 package pe.idat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pe.idat.model.Bodega;
 
+@Repository
 public interface BodegaRepository extends JpaRepository<Bodega, Integer>{
 
 }
